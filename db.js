fb13ca407398a214f87fcd9079c5df07b72f1623
Jath124 <jathsiry.sanchez.itid@unipolidgo.edu.mx>
2026-04-13 01:03:23 -0600

    Add files via upload

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-export const connection = mysql.createPool(
+const connection = mysql.createPool(
     {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306 // Agregamos el puerto por si acaso
     }
 );
 
-export default connection.promise();
+// ESTA ES LA LÍNEA QUE DEBES AGREGAR AL FINAL:
+module.exports = connection.promise();
