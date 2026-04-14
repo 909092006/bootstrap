748c9c927ed811f4f126b449f3f123ffd23a4d53
Jath124 <jathsiry.sanchez.itid@unipolidgo.edu.mx>
2026-04-13 20:58:08 -0600

    Add files via upload

--- db.js
+++ db.js
@@ -3,16 +3,12 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER.trim(), // El .trim() elimina espacios accidentales
+  password: process.env.DB_PASSWORD.trim(),
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 4000,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
   ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true // TiDB requiere SSL activo
+    rejectUnauthorized: false // Esto permite que Render se conecte aunque no reconozca el certificado
   }
 });
 
